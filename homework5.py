b4e466e2c92e832b4a5f2de4357ee6db75422764
katzemzem <zeminaekaterina@gmail.com>
2024-07-23 20:06:09 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2.3, True, 'banana')
+print(immutable_var)
+#immutable_var[0]=6 кортеж неизменяемый объект
+mutable_list = ['banana', 6, 8, True, 'pear']
+mutable_list [0:3] = [0]
+print(mutable_list)
